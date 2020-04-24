8a01a28abd40d117e8f57681519e421017344167
Mario Souto <soutomarios@gmail.com>
2020-04-24 09:58:52 -0300

    Pontapé inicial do projeto

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Bem vindo ao Spotcast</Text>
     </View>
   );
 }
